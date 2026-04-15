e764d183c7c11e2912987dc604e6772a7921e77e
Taufan Augusta <taufan.augusta@protonmail.com>
2026-04-15 21:59:18 +0700

    add `GetTest0` to `TestService`

--- src/MacroSSH.Core/Services/TestService.cs
+++ src/MacroSSH.Core/Services/TestService.cs
@@ -2,6 +2,11 @@ namespace MacroSSH.Core.Services;
 
 public sealed class TestService
 {
+    public string GetTest0()
+    {
+        return "Test 0";
+    }
+
     public string GetTest1()
     {
         return "Test 1";
