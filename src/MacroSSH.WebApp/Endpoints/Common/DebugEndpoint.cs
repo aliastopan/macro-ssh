f3a1e78be16b094ae359972550c0a25c83750309
Taufan Augusta <taufan.augusta@protonmail.com>
2026-04-18 20:14:09 +0700

    add `/test/psql` endpoint

--- src/MacroSSH.WebApp/Endpoints/Common/DebugEndpoint.cs
+++ src/MacroSSH.WebApp/Endpoints/Common/DebugEndpoint.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using MacroSSH.Core.Services;
 using MacroSSH.WebApp.Interfaces;
+using MacroSSH.Core.Repositories;
 
 namespace MacroSSH.WebApp.Endpoints.Common;
 
@@ -9,6 +10,7 @@ public sealed class DebugEndpoint : IEndpointDefinition
     public void DefineEndpoints(WebApplication app)
     {
         app.MapGet("/api/hello", Debug);
+        app.MapGet("/api/test/psql", PsqlTest);
     }
 
     internal IResult Debug([FromServices] SampleService sampleService)
@@ -16,5 +18,16 @@ public sealed class DebugEndpoint : IEndpointDefinition
         var hello =  sampleService.GetHelloWorld();
 
         return Results.Ok(hello);
+    }
+
+    internal async Task<IResult> PsqlTest([FromServices] Repository repository)
+    {
+        bool isConnected =  await repository.TestConnectionAsync();
+
+        string result = isConnected
+            ? "PSQL CONNECTED."
+            : "PSQL NOT CONNECTED.";
+
+        return Results.Ok(result);
     }
 }
